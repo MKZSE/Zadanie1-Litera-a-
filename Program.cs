8355940dbbed9bd262d668a817c9d56a2f1620b3
MKZSE <mmkosowskii@gmail.com>
2025-02-21 11:23:11 +0100

    Litera a

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Zadanie1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string wiersz;
+            int powtorzeniaa = 0;
+            const string sciezka = "C:\\test/test_mat_kos.txt";
+            if (!File.Exists(sciezka)) return;
+            StreamReader reader = new StreamReader(sciezka);
+
+            wiersz = reader.ReadLine();
+            while (wiersz != null)
+            {
+                foreach (char znak in wiersz)
+                {
+                    if (znak == 'a')
+                    {
+                        powtorzeniaa++;
+                    }
+                }
+                wiersz = reader.ReadLine();
+            }
+            reader.Close();
+            Console.Write("Litera \'a\' występuje:" + powtorzeniaa);
+            Console.WriteLine();
+        }
+    }
+}
